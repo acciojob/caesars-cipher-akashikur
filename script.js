390b72f5344253246b1c18c865befe279b30b79c
Akash M <108210779+akashikur@users.noreply.github.com>
2023-06-26 19:01:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function rot13(encodedStr) {
     return char;
   // Only change code below this line
 
-  return; //return decodedArr
+  // return; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
